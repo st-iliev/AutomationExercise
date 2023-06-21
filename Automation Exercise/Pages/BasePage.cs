c19f966d63dfcb9f918a9840dd6b3bc02ecc05e2
Stanislav Iliev <st.d.iliev@gmail.com>
2023-06-21 22:56:34 +0300

    Added LoginPage , ProductsPage and BasePage

---
+++ Automation Exercise/Pages/BasePage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_Exercise.Pages
+{
+    public abstract class BasePage
+    {
+            private int elementsTimeout = 10;
+            protected IWebDriver driver { get; set; }
+            public BasePage(IWebDriver driver)
+            {
+                this.driver = driver;
+                waitDriver = new WebDriverWait(driver, TimeSpan.FromSeconds(elementsTimeout));
+            }
+            protected WebDriverWait waitDriver { get; set; }
+            public abstract string PageURL { get; }
+            public void Open()
+            {
+                driver.Navigate().GoToUrl(PageURL);
+                
+            }
+            public string GetPageTitle() => driver.Title;
+
+    }
+}
---
+++ Automation Exercise/Pages/LoginPage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_Exercise.Pages
+{
+    public class LoginPage : BasePage
+    {
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public override string PageURL => "https://www.automationexercise.com/login";
+        public IWebElement loginEmailField => driver.FindElement(By.XPath("input[@data-qa='login-email']"));
+        public IWebElement loginPasswordField => driver.FindElement(By.XPath("input[@data-qa='login-password']"));
+        public IWebElement loginButton => driver.FindElement(By.XPath("button[@data-qa='signup-button']"));
+        public IWebElement signupNameField => driver.FindElement(By.XPath("input[@data-qa='signup-name']"));
+        public IWebElement signupEmailField => driver.FindElement(By.XPath("input[@data-qa='signup-email']"));
+        public IWebElement signupButton => driver.FindElement(By.XPath("button[@data-qa='signup-button']"));
+    }
+}
