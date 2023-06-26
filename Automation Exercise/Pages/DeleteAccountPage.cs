13e64b63af5f3eb39ff64bad79b4ca0ecf85f459
Stanislav Iliev <st.d.iliev@gmail.com>
2023-06-26 23:14:50 +0300

    Craete new Account Created and Delete Pages, add new locators in Base and Singup Page, crate new utilities classes called constants and succesfulmessages.

---
+++ Automation Exercise/Pages/DeleteAccountPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_Exercise.Pages
+{
+    internal class DeletedAccountPage
+    {
+    }
+}
